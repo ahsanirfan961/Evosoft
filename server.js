248f64a51e0b3d018ab68090f2f39fa3200f9267
Muhammad Ahsan <ahsanirfan961@gmail.com>
2024-08-06 22:40:30 +0500

    Routing Setup

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
-const express = require('express')
+import express from "express";
+import path from "path";
+import { fileURLToPath } from 'url';
 
-const app = express()
+const app = express();
+const port = 3000;
+
+// Create __dirname equivalent
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname)));
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(3000, )
+app.get("/verify", function (req, res) {
+    res.sendFile(path.join(__dirname, "verify.html"));
+})
+
+app.get("/login", function (req, res) {
+    res.sendFile(path.join(__dirname, "login.html"));
+})
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
